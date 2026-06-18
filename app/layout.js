f3a993988ef80d7b5c10dd2bbfa1f121faab0d91
Devansh Sahu <sahudevansh21@gmail.com>
2026-06-18 12:41:59 +0530

    Add app/layout.js

---
+++ app/layout.js
@@ -0,0 +1,12 @@
+import './globals.css';
+
+export default function Layout({ children }) {
+  return (
+    <html lang="en">
+      <head>
+        <script src="https://cdn.tailwindcss.com"></script>
+      </head>
+      <body className="antialiased">{children}</body>
+    </html>
+  );
+}
